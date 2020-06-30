e7403ff632a09482255919a7ff3e52352d29832d
Benjamin REZEL <benjamin.rezel@orange.fr>
2020-07-01 01:02:37 +0200

    Create stylesheet.php

---
+++ views/includes/stylesheet.php
@@ -0,0 +1,18 @@
+<?php 
+
+function registerStylesheet(){
+    $manifest = file_get_contents("http://localhost/coffeedev/assets/js/manifest.json");
+    $css = json_decode($manifest, JSON_FORCE_OBJECT);
+    $css['theme.css'] = substr($css['theme.css'],7);
+
+    $styles = [
+        'main-theme-css' => '<link rel="stylesheet" href="http://localhost/coffedev/assets/css/'.$css['theme.css'].'">',
+        'main-theme-sourcemap-css' => '<link rel="stylesheet" href="http://localhost/coffeedev/assets/css/'.$css['theme.css.map'].'">'
+    ];
+    
+    foreach($styles as $style){
+        echo $style;
+    }
+}
+
+registerStylesheet();
