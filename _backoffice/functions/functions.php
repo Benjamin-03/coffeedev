7bea8aeb0f2f33d232c5e3255734d85460802ce2
Benjamin REZEL <benjamin.rezel@orange.fr>
2020-07-02 00:32:42 +0200

    correction function

--- _backoffice/functions/functions.php
+++ _backoffice/functions/functions.php
@@ -13,19 +13,18 @@ function str_secure($string){
  * Permet de rendre plus lisible le debug
  * @param $var
  */
-function debug($var){
+function debug($var)
+{
     echo '<pre>';
         var_dump($var);
     echo '</pre>';
 }
-function getMessage ()
-{
-if(isset($_GET{'error'}) && isset($_GET{'message'}) ) 
-{
-    echo '<div class="alert alert-danger"> '.$_GET{'message'}.'</div>';
-}elseif(isset($_GET {'success'}) && isset($_GET{'message'}))
-{
-    echo '<div class="alert alert-success"> '.$_GET{'message'}.'</div>';
-}
 
+function getMessage()
+{
+    if(isset($_GET['error']) && isset($_GET['message'])) {
+        echo '<div class="alert alert-danger"> '.$_GET['message'].'</div>';
+    } else if(isset($_GET['success']) && isset($_GET['message'])) {
+        echo '<div class="alert alert-success">'.$_GET['message'].'</div>';
+    }
 } 
